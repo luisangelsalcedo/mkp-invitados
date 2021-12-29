27f07c2e8be7fc647af8ca1d47a308147216133f
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2021-12-28 19:08:02 -0500

    add Administrar Invitados

--- src/App.js
+++ src/App.js
@@ -1,45 +1,77 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-6 col-sm-offset-3">
-            <form>
-              <div className="form-group">
-                <label htmlFor="first-name">Nombre</label>
-                <input type="text" className="form-control" name="first-name" />
-              </div>
+const App = () => {
+  const [invitados, setInvitados] = useState([]);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const handlerSubmit = event => {
+    event.preventDefault();
+    if (firstName.length > 0 && lastName.length > 0) {
+      setInvitados([
+        {
+          fName: firstName,
+          lName: lastName,
+        },
+        ...invitados,
+      ]);
+      setFirstName("");
+      setLastName("");
+    }
+  };
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-6 col-sm-offset-3">
+          <form onSubmit={e => handlerSubmit(e)}>
+            <div className="form-group">
+              <label htmlFor="first-name">Nombre</label>
+              <input
+                type="text"
+                className="form-control"
+                name="first-name"
+                value={firstName}
+                onChange={e => setFirstName(e.target.value)}
+              />
+            </div>
 
-              <div className="form-group">
-                <label htmlFor="last-name">Apellido</label>
-                <input type="text" className="form-control" name="last-name" />
-              </div>
+            <div className="form-group">
+              <label htmlFor="last-name">Apellido</label>
+              <input
+                type="text"
+                className="form-control"
+                name="last-name"
+                value={lastName}
+                onChange={e => setLastName(e.target.value)}
+              />
+            </div>
 
-              <div className="action">
-                <button type="submit" className="btn btn-primary">Agregar Invitado</button>
-              </div>
-            </form>
+            <div className="action">
+              <button type="submit" className="btn btn-primary">
+                Agregar Invitado
+              </button>
+            </div>
+          </form>
 
-            <table className="table bordered-table table-striped">
-              <thead>
-                <tr>
-                  <th>Nombre</th>
-                  <th>Apellido</th>
+          <table className="table bordered-table table-striped">
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Apellido</th>
+              </tr>
+            </thead>
+            <tbody>
+              {invitados.map(({ fName, lName }, idx) => (
+                <tr key={String(idx)}>
+                  <td>{fName}</td>
+                  <td>{lName}</td>
                 </tr>
-              </thead>
-              <tbody>
-
-              </tbody>
-            </table>
-          </div>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
-    )
-  }
-}
+    </div>
+  );
+};
 
-export default App
-
-
+export default App;
